3cf6a2b0526fcf5f7b55c31348f59152194f4ff1
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-18 13:02:37 -0500

    Replaced stock homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@
 require 'sidekiq/web'
 
 CosmicSunsetNight::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#root'
 
   mount Sidekiq::Web, at: '/sidekiq'
 
