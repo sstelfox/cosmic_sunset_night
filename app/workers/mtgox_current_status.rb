466ec81d89d5989c5cbedeb9a0475b2f7921a3b6
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-18 23:34:56 -0500

    Began working on the actual worker

--- app/workers/mtgox_current_status.rb
+++ app/workers/mtgox_current_status.rb
@@ -1,15 +1,18 @@
 
-require 'mtgox'
+require 'net/https'
 
 class MtgoxCurrentStatus
   include Sidekiq::Worker
 
-  def perform
-    ru = URI.parse(ENV['REDIS_PROVIDER'])
-    ru.path = '/0'
-    redis = Redis.new(url: ru.to_s)
+  sidekiq_options :retry => false
 
-    redis.publish('test', 'message')
+  URL = 'https://data.mtgox.com/api/2/BTCUSD/money/ticker'
+
+  def perform
+    current_data = JSON.parse(Net::HTTP.get(URI.parse(URL)))["data"]
+    time_stamp = current_data["now"].to_i
+
+    Redis.current.publish('test', 'message')
   end
 end
 
