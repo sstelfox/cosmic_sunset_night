69d6cf009d1cc0c92c356790006df0af7683fac2
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-19 00:29:57 -0500

    Think I have my source data working...

--- app/workers/mtgox_current_status.rb
+++ app/workers/mtgox_current_status.rb
@@ -1,23 +1,34 @@
 
 require 'net/https'
 
+
 class MtgoxCurrentStatus
   include Sidekiq::Worker
 
   sidekiq_options :retry => false
 
+  LIMIT = 172800
   URL = 'https://data.mtgox.com/api/2/BTCUSD/money/ticker'
 
   def perform
     current_data = JSON.parse(Net::HTTP.get(URI.parse(URL)))["data"]
     time_stamp = current_data["now"].to_i
+    metrics = ['avg', 'buy', 'high', 'last', 'low', 'sell', 'vol']
 
     r = Redis.new(url: ENV['REDIS_PROVIDER'])
-    r.multi do
-      ['avg', 'buy', 'high', 'last', 'low', 'sell'].each do |k|
-        redis.zadd("mtgox:#{k}", time_stamp, current_data[k]['value_int'])
+
+    sizes = r.multi do
+      metrics.each do |k|
+        r.zcard("mtgox:#{h}")
       end
-      redis.zadd('mtgox:vol', time_stamp, current_data['vol']['value'])
+    end
+    size_map = metrics.zip(sizes.map(&:to_i))
+
+    r.multi do
+      metrics.each do |k|
+        z.remrangebyrank("mtgox:#{k}", 0, LIMIT * -1) if size_map[k] >= LIMIT
+        r.zadd("mtgox:#{k}", time_stamp, current_data[k]['value'])
+      end
     end
   end
 end
