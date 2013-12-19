3b67f8dfaabfe68c0efa537bd8877e9d14963316
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-18 23:52:31 -0500

    Now querying mtgox

--- app/workers/mtgox_current_status.rb
+++ app/workers/mtgox_current_status.rb
@@ -12,7 +12,16 @@ class MtgoxCurrentStatus
     current_data = JSON.parse(Net::HTTP.get(URI.parse(URL)))["data"]
     time_stamp = current_data["now"].to_i
 
-    Redis.current.publish('test', 'message')
+    r = Redis.new(url: ENV['REDIS_PROVIDER'])
+    r.multi do
+      redis.zadd('mtgox:avg', time_stamp, current_data['avg']['value_int'])
+      redis.zadd('mtgox:buy', time_stamp, current_data['buy']['value_int'])
+      redis.zadd('mtgox:high', time_stamp, current_data['high']['value_int'])
+      redis.zadd('mtgox:last', time_stamp, current_data['last']['value_int'])
+      redis.zadd('mtgox:low', time_stamp, current_data['low']['value_int'])
+      redis.zadd('mtgox:sell', time_stamp, current_data['sell']['value_int'])
+      redis.zadd('mtgox:vol', time_stamp, current_data['vol']['value'])
+    end
   end
 end
 
