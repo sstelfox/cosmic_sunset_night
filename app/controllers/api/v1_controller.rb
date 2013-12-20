5c75414e7f46df1d4dabff2e1d94bcfc8f9dd5f1
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-20 16:32:36 -0500

    Fixed the API to return hashes of data

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1Controller < ApplicationController
   def trade_data
     metrics = ['avg', 'buy', 'high', 'last', 'low', 'sell', 'vol', 'vwap']
-    @time_series = $redis.multi do
+    values = $redis.multi do
       metrics.each do |m|
         $redis.lrange("mtgox:#{m}", 0, 100)
       end
     end
+    @time_series = metrics.zip(values)
   end
 end
