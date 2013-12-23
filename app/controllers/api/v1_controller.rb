4f983a93c16528bee9e8b23ba336aa51d0b00937
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-23 17:00:30 -0500

    Limited the API to 250 entries at the time

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1Controller < ApplicationController
   def trade_data
     date = $redis.sort('mtgox:ticker:periods', {order: 'DESC', limit: [0, 1]}).first
-    @time_series = $redis.zrange("mtgox:ticker:#{date}", 0, -1).map { |i| JSON.parse(i) }
+    @time_series = $redis.zrange("mtgox:ticker:#{date}", 0, 250).map { |i| JSON.parse(i) }
   end
 end
